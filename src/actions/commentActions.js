4903fbf3ba02fcb5752077dfcb9fb7988a48afb8
Steven <steven_moxon123@hotmail.co.uk>
2017-10-29 00:09:16 +0100

    [commentActions.js] implement request action

--- src/actions/commentActions.js
+++ src/actions/commentActions.js
@@ -1,20 +1,36 @@
 import * as types from '../types';
 import axios from 'axios';
+const ROOT = 'http://localhost:3080/';
 
-export function fetchCommentsRequest () {
+export function fetchAllComments(id) {
+	return function (dispatch) {
+		dispatch(fetchCommentsRequest());
+		axios
+			.get(`${ROOT}/articles/${id}/comments`)
+			.then((res) => {
+				dispatch(fetchCommentsSuccess(res.data.comments));
+			})
+			.catch((err) => {
+				dispatch(fetchCommentsFailure(err));
+			});
+	};
+}
+
+
+export function fetchCommentsRequest() {
 	return {
 		type: types.FETCH_COMMENTS_REQUEST,
 	};
 }
 
-export function fetchCommentsFailure (payload) {
+export function fetchCommentsFailure(payload) {
 	return {
 		type: types.FETCH_COMMENTS_FAILURE,
 		payload: payload
 	};
 }
 
-export function fetchCommentsSuccess (payload) {
+export function fetchCommentsSuccess(payload) {
 	return {
 		type: types.FETCH_COMMENTS_SUCCESS,
 		payload: payload
