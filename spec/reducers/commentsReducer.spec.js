103aad993bcb0b53ad1adc39239291537672e32f
Steven <steven_moxon123@hotmail.co.uk>
2017-10-28 21:36:09 +0100

    seperate out reducer tests to different files

---
+++ spec/reducers/commentsReducer.spec.js
@@ -0,0 +1,34 @@
+import {expect} from 'chai';
+import {fetchComments, fetchCommentsSuccess, fetchCommentsFailure} from '../../src/actions/commentActions';
+import {initialState, commentsReducer} from '../../src/reducers/commentsReducer';
+
+describe('COMMENTS REDUCER', () => {
+    describe('action: fetchComments', () => {
+        it('should update the state correctly for fetchComments', () => {
+            const action = fetchComments();
+            const newstate = commentsReducer(initialState, action);
+            expect(initialState).to.not.eql(newstate);
+        });
+        it('should update the state correctly for fetchCommentsSuccess', () => {
+            const data = [
+                { _id: 30, title: 'coding' },
+                { _id: 12, title: 'cooking' },
+                { _id: 122, title: 'football' }
+                  ];
+              const exp = {
+                '30': { _id: 30, title: 'coding' },
+                '12': { _id: 12, title: 'cooking' },
+                '122': { _id: 122, title: 'football' }
+              };
+            const action = fetchCommentsSuccess(data);
+            const newstate = commentsReducer(initialState, action);
+            expect(initialState).to.not.eql(newstate);
+            expect(newstate.comments).to.eql(exp);
+        });
+        it('should update the state correctly for fetchCommentsFailure', () => {
+            const action = fetchCommentsFailure('error');
+            const newstate = commentsReducer(initialState, action);
+            expect(initialState).to.not.eql(newstate);
+        });
+    });
+});
---
+++ spec/reducers/topicsReducer.spec.js
@@ -0,0 +1,34 @@
+import {expect} from 'chai';
+import {fetchTopics, fetchTopicsSuccess, fetchTopicsFailure} from '../../src/actions/topicActions';
+import {initialState, topicsReducer} from '../../src/reducers/topicsReducer';
+
+describe('TOPICS REDUCER', () => {
+    describe('action: fetchTopics', () => {
+        it('should update the state correctly for fetchTopics', () => {
+            const action = fetchTopics();
+            const newstate = topicsReducer(initialState, action);
+            expect(initialState).to.not.eql(newstate);
+        });
+        it('should update the state correctly for fetchTopicsSuccess', () => {
+            const data = [
+                { _id: 30, title: 'coding' },
+                { _id: 12, title: 'cooking' },
+                { _id: 122, title: 'football' }
+                  ];
+              const exp = {
+                '30': { _id: 30, title: 'coding' },
+                '12': { _id: 12, title: 'cooking' },
+                '122': { _id: 122, title: 'football' }
+              };
+            const action = fetchTopicsSuccess(data);
+            const newstate = topicsReducer(initialState, action);
+            expect(initialState).to.not.eql(newstate);
+            expect(newstate.topics).to.eql(exp);
+        });
+        it('should update the state correctly for fetchTopicsFailure', () => {
+            const action = fetchTopicsFailure('error');
+            const newstate = topicsReducer(initialState, action);
+            expect(initialState).to.not.eql(newstate);
+        });
+    });
+});
