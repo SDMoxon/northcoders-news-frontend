2eaa375612835f8d95f320bcfe2e027ed259c369
Steven <steven_moxon123@hotmail.co.uk>
2017-10-29 20:47:42 +0000

    [componentApp] make path exact for /articles/:articleId

--- src/components/App.js
+++ src/components/App.js
@@ -15,7 +15,7 @@ class App extends Component {
             <Header />
             <Switch>
               <Route exact path="/" component={Articles} />
-              <Route path="/articles/:articleId" component={Articles}/>
+              <Route exact path="/articles/:articleId" component={Articles}/>
               <Route exact path="/topics/:id/articles" component={Articles} />
               <Route component={Error} />
             </Switch>
