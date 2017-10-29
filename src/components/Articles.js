310202a44b776ab37ca897b4bed86a72b3293821
Steven <steven_moxon123@hotmail.co.uk>
2017-10-29 13:23:20 +0000

    [components/Articles.js]add toggle to show and hide comments

--- src/components/Articles.js
+++ src/components/Articles.js
@@ -1,12 +1,22 @@
 import React, { Component } from 'react';
 import Comments from './Comments';
 import { connect } from 'react-redux';
-import { fetchAllArticles } from '../actions/articleActions';
+import { fetchAllArticles, changeCommentVisability } from '../actions/articleActions';
 import { map } from 'underscore';
 
 class Articles extends Component {
+    constructor(props) {
+        super(props);
+        this.handleClick = this.handleClick.bind(this);
+    }
     componentDidMount() {
         this.props.getArticles();
+        console.log('*********',this);
+    }
+    handleClick(e) {
+        console.log('8888888888',this);
+        const articleId = e.target.value;
+        this.props.commentVisability(articleId);
     }
     render() {
         return (
@@ -15,10 +25,17 @@ class Articles extends Component {
                 {Object.keys(this.props.articles.articles).length ?
                     map(this.props.articles.articles, (article) => {
                         return (
-                        <div key = {article._id}>
-                        <div className='row' >{article.title}</div>
-                        <Comments belongsTo={article._id}/>
-                        </div>
+                            <div key={article._id}>
+                                <div className='row' >{article.title}</div>
+                                {this.props.articles.articles[article._id].commentVisable ?
+                                    <div>
+                                        <button onClick={this.handleClick} value={article._id}>Hide Comments</button>
+                                        <Comments belongsTo={article._id} />
+                                    </div>
+                                    :
+                                    <button onClick={this.handleClick} value={article._id}>Show Comments</button>
+                                }
+                            </div>
                         );
                     }) : 'Loading'
 
@@ -31,6 +48,9 @@ function mapDispatchToProps(dispatch) {
     return {
         getArticles: () => {
             dispatch(fetchAllArticles());
+        },
+        commentVisability: (id) => {
+            dispatch(changeCommentVisability(id));
         }
     };
 }
