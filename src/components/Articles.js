c59b88013dcd3962a211872f4fb82a86db5bcd6e
Steven <steven_moxon123@hotmail.co.uk>
2017-10-30 11:07:33 +0000

    [componentsArticles] add additional information to component

--- src/components/Articles.js
+++ src/components/Articles.js
@@ -15,7 +15,7 @@ class Articles extends Component {
         const currentTopic = this.props.match.params.id;
         const nextTopic = nextProps.match.params.id;
 
-        if (nextProps.match.params.articleId !== this.props.match.params.articleId && 
+        if (nextProps.match.params.articleId !== this.props.match.params.articleId &&
             nextProps.match.params.articleId !== undefined) {
             this.props.getArticle(nextProps.match.params.articleId);
         }
@@ -38,17 +38,25 @@ class Articles extends Component {
     conditionalRender(article) {
         if (Object.keys(this.props.articles.articles).length > 1) {
             return (
-                <div className='row' key={article._id}>
-                    <NavLink to={`/articles/${article._id}`} >{article.title}</NavLink>
-                    <p>{article.body}</p>
+                <div className='row panel panel-info' key={article._id}>
+                    <div className='panel-body'>
+                        <NavLink to={`/articles/${article._id}`} >{article.title}</NavLink>
+                        <p>{article.body}</p>
+                        <p>Votes {article.votes}</p>
+                    </div>
                 </div>
             );
         }
         else {
             return (
-                <div className='row' key={article._id}>
-                    <p>{article.title}</p>
-                    <p>{article.body}</p>
+                <div className='row ' key={article._id}>
+                    <div class='panel panel-info'>
+                        <div class='panel-body'>
+                        <p>{article.title}</p>
+                        <p>{article.body}</p>
+                        <p>Votes {article.votes}</p>
+                        </div>
+                    </div>
                     {
                         this.props.articles.articles[article._id].commentVisable ?
                             <div>
