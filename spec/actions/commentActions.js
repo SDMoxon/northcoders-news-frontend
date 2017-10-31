44e6ba74f8a69d67938946e4ccad2fc1a583c3f5
Steven <steven_moxon123@hotmail.co.uk>
2017-10-31 11:54:05 +0000

    [commentAlterVotes] expand tests to include Requests, Success, Failure

--- spec/actions/commentActions.js
+++ spec/actions/commentActions.js
@@ -42,7 +42,7 @@ describe('Comments Actions', () => {
             const payload = {}
             expect(commentActions.postCommentSuccess(payload)).to.eql({
                 type: types.POST_COMMENTS_SUCCESS,
-                payload:payload
+                payload: payload
             });
         });
 
@@ -50,7 +50,7 @@ describe('Comments Actions', () => {
             const error = 'error'
             expect(commentActions.postCommentFailure(error)).to.eql({
                 type: types.POST_COMMENTS_FAILURE,
-                payload:error
+                payload: error
             });
         });
     });
@@ -67,6 +67,36 @@ describe('Comments Actions', () => {
         it('should return the correct action', () => {
             expect(commentActions.resetCommentState()).to.eql({
                 type: types.RESET_COMMENT_STATE
+            });
+        });
+    });
+    describe.only('commentAlterVotes', () => {
+        describe('commentAlterVotesRequest', () => {
+            it('returns object with correct payload', () => {
+                const payload = 'up';
+                expect(commentActions.commentAlterVotes(payload)).to.eql({
+                    type: types.COMMENT_ALTER_VOTESR_Request,
+                });
+            });
+        });
+        describe('commentAlterVotesSuccess', () => {
+            it('returns object with correct payload', () => {
+                const payload = {
+                    commentId:12345
+                };
+                expect(commentActions.commentAlterVotesSuccess(payload)).to.eql({
+                    type: types.COMMENT_ALTER_VOTES_SUCCESS,
+                    payload: payload
+                });
+            });
+        });
+        describe('commentAlterVotesFailure', () => {
+            it('returns object with correct payload', () => {
+                const payload = 'Error!';
+                expect(commentActions.commentAlterVotesFailure(payload)).to.eql({
+                    type: types.COMMENT_ALTER_VOTES_FAILURE,
+                    payload: payload
+                });
             });
         });
     });
